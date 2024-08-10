b7b852d09832ff55dd1b8fcd35ae2c34e3fb8a8d
AlexandruAndronache <alexandru.andronache21@gmail.com>
2024-08-10 18:06:14 +0300

    Added frequency switch
    using a SWITCH/CASE the clock frequency can be changed using 3 values: 50, 80, 120 MHz

--- HSE_PLL_SYSCLK/Core/Src/main_app.c
+++ HSE_PLL_SYSCLK/Core/Src/main_app.c
@@ -24,6 +24,7 @@ void UART2_Init(void);
 void Error_handler(void);
 void menu(void);
 void SystemClock_Config(uint8_t clock_freq);
+void SystemClock_Config_HSE(uint8_t clock_freq);
 
 
 UART_HandleTypeDef huart2;
@@ -41,7 +42,7 @@ int main(void)
 
 	HAL_Init();
 
-	SystemClock_Config(SYS_CLOCK_FREQ_80_MHZ);
+	SystemClock_Config_HSE(SYS_CLOCK_FREQ_50_MHZ);
 
 	UART2_Init();
 		memset(msg,0,sizeof(msg));
@@ -67,6 +68,96 @@ int main(void)
 
 
 	return 0;
+}
+
+void SystemClock_Config_HSE(uint8_t clock_freq)
+{
+	RCC_OscInitTypeDef osc_init;
+		RCC_ClkInitTypeDef clk_init;
+
+		osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+		osc_init.HSEState = RCC_HSE_BYPASS;
+		osc_init.PLL.PLLState = RCC_PLL_ON;
+		osc_init.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+
+		switch(clock_freq)
+		{
+		case SYS_CLOCK_FREQ_50_MHZ:
+		{
+			osc_init.PLL.PLLM = 8;
+			osc_init.PLL.PLLN = 100;
+			osc_init.PLL.PLLQ = 2;
+			osc_init.PLL.PLLR = 2;
+			osc_init.PLL.PLLP = 2;
+
+		    clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+		    					RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+		    clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+		    clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		    clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+		    clk_init.APB2CLKDivider = RCC_HCLK_DIV1;
+
+		    FLatency = FLASH_ACR_LATENCY_1WS;;
+			break;
+		}
+
+		case SYS_CLOCK_FREQ_80_MHZ:
+		{
+
+			osc_init.PLL.PLLM = 8;
+			osc_init.PLL.PLLN = 160;
+			osc_init.PLL.PLLQ = 2;
+			osc_init.PLL.PLLR = 2;
+			osc_init.PLL.PLLP = 2;
+
+		    clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+		    					RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+		    clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+		    clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		    clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+		    clk_init.APB2CLKDivider = RCC_HCLK_DIV1;
+
+		    FLatency = FLASH_ACR_LATENCY_2WS;
+			break;
+		}
+
+		case SYS_CLOCK_FREQ_120_MHZ:
+		{
+			osc_init.PLL.PLLM = 8;
+			osc_init.PLL.PLLN = 240;
+			osc_init.PLL.PLLQ = 2;
+			osc_init.PLL.PLLR = 1;
+			osc_init.PLL.PLLP = 4;
+
+		    clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+		    					RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+		    clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+		    clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		    clk_init.APB1CLKDivider = RCC_HCLK_DIV4;
+		    clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+
+		    FLatency = FLASH_ACR_LATENCY_3WS;
+			break;
+		}
+
+		default:
+			return;
+		}
+
+		if(HAL_RCC_OscConfig(&osc_init) != HAL_OK)
+			{
+			Error_handler();
+			}
+
+		if(HAL_RCC_ClockConfig( &clk_init, FLatency) != HAL_OK)
+			{
+				Error_handler();
+			}
+
+		//Systick frequency
+		HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
+		HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
 }
 
 void SystemClock_Config(uint8_t clock_freq)
